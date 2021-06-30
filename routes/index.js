04eca69745ce50a962ac04ae8ba9297288e9144c
Rajcognnox <81282047+Rajcognnox@users.noreply.github.com>
2021-06-30 14:44:34 +0530

    first commit

---
+++ routes/index.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var router = express.Router();
+var path= require('path')
+var app= express();
+//app.use(express.static(path.join(__dirname, 'public/')));
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.render('site/index');
+});
+// router.get('/contact', function(req, res, next) {
+//   res.render('site/contact');
+// });
+// router.get('/about', function(req, res, next) {
+//   res.render('site/about');
+// });
+// router.get('/features', function(req, res, next) {
+//   res.render('site/features');
+// });
+// router.get('./whitepaper.pdf', function(req, res, next) {
+//   res.render('site/whitepaper.pdf');
+// });
+// router.get('/login', function(req, res, next) {
+//   res.render('site/login');
+// });
+// router.get('/test', function(req, res, next) {
+//   res.render('site/soon');
+// });
+
+module.exports = router;
